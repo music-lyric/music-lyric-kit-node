70aa504ae685922bd8ce67f62dbabaae7fbb98e5
folltoshe <folltoshe@foxmail.com>
2026-06-18 04:30:24 +0800

    fix(plugin-transform-space): preserve word config when rebuilding spaced words

--- plugins/transform-space/src/core.ts
+++ plugins/transform-space/src/core.ts
@@ -162,6 +162,7 @@ export const insertSpaceToLine = (line: Lyric.LineNormalContent, target: Set<Ins
       normal.time = originalWord.time
       normal.extended = originalWord.extended
       normal.content = matchedText
+      normal.config = originalWord.config
       return normal
     },
     (w) => w.content.replace(/\s/g, ''),
